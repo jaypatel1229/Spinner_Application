504178e800380ed1dd7fc80054d45b2ae11b03e6
jay.patel <jay.patel@1R-80>
2022-02-09 12:04:50 +0530

    Add project files.

---
+++ SpinnerApp/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+
+namespace SpinnerApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private List<KeyValuePair<string, string>> planets;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spn1);
+
+            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            var adapter = ArrayAdapter.CreateFromResource(
+                    this, Resource.Array.planets_array, Android.Resource.Layout.SimpleSpinnerItem);
+
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+
+            planets = new List<KeyValuePair<string, string>>
+{
+    new KeyValuePair<string, string>("Mercury", "167 degrees C"),
+    new KeyValuePair<string, string>("Venus", "464 degrees C"),
+    new KeyValuePair<string, string>("Earth", "15 degrees C"),
+    new KeyValuePair<string, string>("Mars", "-65 degrees C"),
+    new KeyValuePair<string, string>("Jupiter" , "-110 degrees C"),
+    new KeyValuePair<string, string>("Saturn", "-140 degrees C"),
+    new KeyValuePair<string, string>("Uranus", "-195 degrees C"),
+    new KeyValuePair<string, string>("Neptune", "-200 degrees C")
+};
+
+
+            List<string> planetNames = new List<string>();
+            foreach (var item in planets)
+                planetNames.Add(item.Key);
+
+            var adapter1 = new ArrayAdapter<string>(this,
+    Android.Resource.Layout.SimpleSpinnerItem, planetNames);
+
+
+
+
+
+        }
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+            string toast = string.Format("The mean temperature for planet {0} is {1}",
+                spinner.GetItemAtPosition(e.Position), planets[e.Position].Value);
+            Toast.MakeText(this, toast, ToastLength.Long).Show();
+
+
+
+
+
+        }
+    }
+}
